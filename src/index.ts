bf8192d16e5fbcda93613eec18db86ada18fd8c1
stevenzeck <steven.zeck@gmail.com>
2019-08-18 21:49:50 -0500

    Fix read pkg up

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import * as readPkgUp from "read-pkg-up";
 import * as fs from "fs";
 import * as path from "path";
 
-const { pkg } = readPkgUp.sync({ cwd: __dirname });
+const pkg = readPkgUp.sync({ cwd: __dirname })!.package;
 
 export const beautifier: Beautifier = {
   name: "PHP_CodeSniffer",
